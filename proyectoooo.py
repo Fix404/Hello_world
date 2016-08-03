1cf1d34c1a087d664e8dade5889777c8de7120bf
Fix404 <niscolamaximiliano@gmail.com>
2016-08-03 12:34:46 -0300

    Add files via upload
    primer paso probablemente es muy básico pero parece q funciona. modifique el contexto porque me parecía muy infantil

---
+++ proyectoooo.py
@@ -0,0 +1,44 @@
+##Beyond Deep Space (beta)%Nombre provisorio%
+print('Despiertas en una nave espacial que viaja de una galaxia a otra. Tu nombre es Walter White (?) y la cabeza te da vueltas. Cuando uno piensa en explorar los misterios del Universo imagina un vehículo de avanzada con todas las comodidades para sus tripulantes, pero al mirar alrededor el panorama es muy distinto; piso y paredes de metal oxidado, una calefacción peligrosamente defectuosa y un líquido púrpura gotenado del techo.')
+print('El punto es que este viaje no es una exploración; es un exilio. Miles de generaciones provenientes de otros mundos han pasado por esos mismos pasillos (sí, y por tu misma habitación) con un objetivo común: encontrar un nuevo hogar.')
+print('Precisamente esa es tu tarea: viajar a planetas que puedan albergar vida y evaluar las condiciones que presentan: atmósfera, recursos naturales, nivel de hostilidad de los lugareños, esa clase de cosas.')
+print('Te acercas de improviso al monitor de tu escritorio para revisar tus mensajes y resulta que has recibido informes de misión reportando dos nuevos candidatos: Phonos (de la galaxia Deios) y Zedna (galaxia Uohm).')
+print('Sabes que nadie envidia tu trabajo. Tal vez destapar cloacas o preparar las raciones en la cocina de la nave no sean las actividades más excitantes, pero las bajas probabilidades de ser devorado vivo o caer en una grieta de varios kilómetros de profundidad compensan el aburrimiento.')
+print('Te vistes, sales de la habitación y por un ascensor interno llegas a la plataforma de despegue. Entonces...¿Adónde vamos?')
+
+
+
+print('Phonos: planeta árido con amplios yacimientos minerales de fácil acceso. Atmósfera con oxígeno y presencia de tribus nómadas primitivas.')
+
+print('Zedna: planeta trópical con alta diversidad orgánica. Dificil acceso y selva insodeable. Se desconoce presencia de vida inteligente.')
+
+
+d1=input('Ingrese 1 para establecer a Phonos como destino, ó 2 establecer a Zedna como destino;  ')
+if d1 == 1:
+    print('Tu viaje a Phonos comienza...')
+else:
+  print ('Te diriges a Zedna...')
+
+
+##      'Cuando uno piensa en explorar los misterios del Universo
+##'Por el hecho de haber nacido en el espacio
+##'puedes escoger a que galaxia y pianeta deseas
+##'pertenecer y poseer su ciudadanfa.
+##'Como la nave espacial viaja a muchfsima ve-
+##'locidad, alcanzas la edad terrestre de dieciocho
+##'anos en solo tres dfas y dos horas. Ahora debes
+##'elegir entre el pianeta Phonon de la galaxia de
+##'Pineum y el pianeta Zermacroyd de la galaxia
+##'de OOPHOSS.
+##'Phonon tiene tres veces el tamafio del piane-
+##'ta Tierra. La estrella que le proporciona una
+##'parte de su fuerza vital es enorme pero antigua.
+##'Se teme que est6 perdiendo su fuerza.
+##'Zermacroyd estS en la galaxia OOPHOSS. Es
+##'una zona de navegacion dificil y sus agujeros
+##'negros, que no se hallan en los mapas, son pelL
+##'grosos. Los informes de las pruebas espaciales
+##'previas dicen que Zermacroyd ha tenido un pa-
+##'sado oscuro y problem&tico. Los informes tam-
+##'bien profetizan un futuro brillante y estimulante.')
+
